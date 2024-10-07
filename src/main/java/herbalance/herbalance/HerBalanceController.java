bc94f193afd3a1dc0c1faf6026ac33a5cbf95ef5
damorelo115 <damolo@farmingdale.edu>
2024-10-06 21:52:10 -0400

    Name Entering Program

--- src/main/java/herbalance/herbalance/HerBalanceController.java
+++ src/main/java/herbalance/herbalance/HerBalanceController.java
@@ -1,14 +1,27 @@
 package herbalance.herbalance;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+public class HerBalanceController {
     @FXML
-    private Label welcomeText;
+    private Label nameLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nameField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    protected void onButtonClick() {
+        String name = nameField.getText();
+        if (!name.isEmpty()) {
+            nameLabel.setText("Hello, " + name + "!");
+        } else {
+            nameLabel.setText("Please enter your name.");
+        }
     }
 }
