96deef1114430f9cdd2082e519612bee7af89a86
SANCS6 <sanchitasuha@gmail.com>
2024-10-16 16:23:56 -0400

    Dashboard issue

---
+++ src/main/java/herbalance/herbalance/Dashboard.java
@@ -0,0 +1,34 @@
+package herbalance.herbalance;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+public class Dashboard extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IIOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Dashboard App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main (String[] args){
+        launch(args);
+    }
+
+}
+
